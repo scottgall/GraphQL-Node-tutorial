38ecfdd20f4ffdb903d9f0a64d38f6eef26a785e
Scott Gall <scottarthurgall@gmail.com>
2019-05-19 12:55:33 -0400

    added link query and resolvers

--- src/index.js
+++ src/index.js
@@ -4,12 +4,32 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
     type Query {
         info: String!
+        feed: [Link!]!
+    }
+    
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
     }`
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
 
 // 2
 const resolvers = {
     Query: {
-        info: () => 'hi betch'
+        info: () => 'hi betch',
+        feed: () => links,
+    },
+
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
     }
 }
 
